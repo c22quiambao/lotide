d5aa0336526394472b4cb9a58acd5659aa6ed8e9
c22quiambao <christelquiambao@gmail.com>
2023-11-16 15:02:19 -0800

    updated code

--- eqArrays.js
+++ eqArrays.js
@@ -26,7 +26,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
