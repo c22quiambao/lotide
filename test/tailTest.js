402fb50aa63ebcfea05b9d4763c0d377903fc068
c22quiambao <christelquiambao@gmail.com>
2023-11-21 12:34:27 -0800

    fixing typos

--- test/tailTest.js
+++ test/tailTest.js
@@ -17,16 +17,16 @@ assertEqual(arr1.length, 4); // ensure we get back 4 elements
 assertEqual(numbers.length, 5); // ensure we get back 5 elements
 assertEqual(arr1[0], 4); // ensure first element is 4
 assertEqual(arr1[1], 3); // ensure second element is 3
-assertEqual(arr1[2], 2); // ensure second element is 2
-assertEqual(arr1[3], 1); // ensure second element is 1 */
+assertEqual(arr1[2], 2); // ensure third element is 2
+assertEqual(arr1[3], 1); // ensure fourth element is 1
 
 // Test Case 3
 const letter = ['a'];
 let arr2 = tail(letter);
-assertEqual(letter.length, 1); // ensure we get back 1 elements
-assertEqual(arr2.length, 0); // ensure first element is null
+assertEqual(letter.length, 1); // ensure we get back 1 element
+assertEqual(arr2.length, 0); // array is empty
 
 // Test Case 3
 const emptyArray = [];
 let arr3 = tail(emptyArray);
-assertEqual(arr3.length, 0); // ensure first element is null
+assertEqual(arr3.length, 0); // array is empty
